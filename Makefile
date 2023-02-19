f8d2de171464ed9f4b35bf30605d920d607c3326
Puddingfoot Dagos <Puddingfoot Dagos@example.com>
2023-02-19 21:25:45 +0300

    LAB_2-P12/donotwork

--- Makefile
+++ Makefile
@@ -1,14 +1,24 @@
 
-all: xfloor format
+all: pyramid format
 
-xfloor: xfloor.c
-	cc -o build/xfloor xfloor.c -lX11 
+pyramid: pyramid0.o pyramid1.o pyramid2.o 
+	cc -o build/pyramid pyramid0.o pyramid1.o pyramid2.o -lX11 
+
+pyramid0.o: pyramid0.c polyhedron.h
+	cc -c pyramid0.c -lX11
+
+pyramid1.o: pyramid1.c polyhedron.h
+	cc -c pyramid1.c -lX11
+
+pyramid2.o: pyramid2.c polyhedron.h
+	cc -c pyramid2.c -lX11
+
 
 format:
-	clang-format -i *.c
+	clang-format -i *.c *.h
 
 clean:
-	rm -rf build/*
+	rm -rf *.o build/*
 
 rebuild: clean all
 
---
+++ polyhedron.h
@@ -0,0 +1,66 @@
+#ifndef POLYHEDRON_H_
+#define POLYHEDRON_H_
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#define MB0 5      /* 4-угольная внешняя грань */
+#define NF3 4       /* 4 3-угольные внутренние грани */
+/* #define NF4 0 */ /* 0 4-угольных внутренних граней */
+/* #define NF5 0 */ /* 0 5-угольных внутренних граней */
+/* #define NF6 0 */ /* 0 6-угольных внутренних граней */
+/* #define NF7 0 */ /* 0 7-угольных внутренних граней */
+/* #define NF8 0 */ /* 0 8-угольных внутренних граней */
+#define NCOLOR 4
+#define DEFTONE 0 /* номер цвета грани по умолчанию */
+#define VDOT 8    /* диаметр вершин графа */
+#define EWIDTH 2  /* толщина ребер графа (<VDOT) */
+#define NUNIT 8   /* диапазон градуировка схемы  %8=0*/
+#define NFACE                                               \
+    (NF3) /* (NF3+ NF4+ NF5                                 \
+                      + NF6+ NF7+NF8)                       \
+#define NEDGE ((3*NF3 + MB0)/2)  /* ((3*NF3 + 4*NF4         \
+                                            + 5*NF5 + 6*NF6 \
+                                            + 7*NF7 + 8*NF8 \
+                                            + MB0)/2) */
+#define NEDGE                                                             \
+    ((3 * NF3 + MB0) / 2) /* ((3*NF3 + 4*NF4                              \
+                                                          + 5*NF5 + 6*NF6 \
+                                                          + 7*NF7 + 8*NF8 \
+                                                          + MB0)/2) */
+
+#define NVERT (NEDGE - (NFACE + 1) + 2)
+typedef XPoint XVertex;
+typedef XSegment XEdge;
+
+typedef struct { /* Структура грани */
+    XPoint* top; /* Адрес набора вершин */
+    int Cn;      /* Число вершин */
+    int tone;    /* Номер цвета */
+    Region regi; /* Региональная зона */
+} XFace;
+typedef struct {     /* Структура многоугольного графа */
+    XVertex* vertex; /* Адрес массива вершин */
+    XEdge* edge;     /* Адрес массива ребер */
+    XFace* face;     /* Адрес массива граней */
+} XPolyGraph;
+
+int assoc(XPolyGraph*);
+GC congraph(Display*);
+Window wingraph(Display*, char*);
+int colorite(Display*);
+int regraph(Display*, Window, GC, int);
+int reset(Display*, Window, int);
+int reface(Display*, Window, GC, int);
+/*Дисплейный модуль (pyramid2) */
+int relink(XPolyGraph*);
+int retrace();
+int resize(unsigned, unsigned);
+int rescale(unsigned, unsigned);
+int rebuild();
+int reconf(unsigned, unsigned);
+int zotone(int, int);
+/*Контрольный модуль (pyramid0) */
+int rekey(XEvent*);
+int dispatch(Display*, Window, GC);
+int main(int argc, char* argv[]);
+Window canvas(Display* dpy);
+#endif
