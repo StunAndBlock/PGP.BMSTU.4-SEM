aa3e9f1c376a2af16b9d2181aff2c28f8837c90b
StunAndBlock <StunAndBlock@example.com>
2023-03-09 14:28:39 +0300

    PAYED/W21-added-skele

---
+++ W21/W21.c
@@ -0,0 +1,33 @@
+#include "W21.h"
+#include <stdio.h>
+
+
+
+
+
+int main(unsigned short argc, char* argv[]){
+if(argc < 2){
+    inform("no boxes inputted\nEXMAPLE: ./<programm_name> 14x23 143x10\n",-1);s
+}
+
+
+
+
+
+    return 0;
+}
+
+
+
+void inform(const char* str, char status){
+    switch(status){
+        case 1:
+    fprintf(stderr,"%s",str);
+    break;
+    case -1:
+    fprintf(stderr,"%s",str);
+    exit(status);
+    default:
+    break; 
+    }
+}
---
+++ W21/W21.h
@@ -0,0 +1,17 @@
+#ifndef W21_H_
+#define W21_H_
+#pragma once
+#include <X11/Xlib.h>
+
+typedef struct winHandl{
+Display* dpy;
+int scr;
+int depth;
+Window root;
+Window* boxes;
+} winHandl; 
+
+void inform(const char*, char) ;
+
+
+#endif /*W21_H_*/
